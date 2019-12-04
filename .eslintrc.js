3ed03179c42a1650dffceee52b4fed247c9ad4c4
yuliagushcha <Yulia_Gushcha@mail.ru>
2019-12-04 10:59:43 +0300

    feat: numbers-tasks complete

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
   rules: {
     "linebreak-style": 0,
     "no-plusplus": "off",
-    "no-else-return": "off"
+    "no-else-return": "off",
+    "no-restricted-properties": "off"
   },
 };
